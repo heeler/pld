27e160e155b9bc499447eaaea6257fe3758a9a7b
heeler <jamie.sherman@alleninstitute.org>
2024-11-12 15:15:25 -0800

    update scripts

--- AI2/build.sh
+++ AI2/build.sh
@@ -6,4 +6,4 @@
 rm -rf ./artifacts/*
 cp entrypoint.sh ./artifacts
 
-beaker image create --name pytorch_dpp_1 "$(docker build -q --file Dockerfile ./artifacts --platform linux/amd64)"
+beaker image create --name pld_1 "$(docker build -q --file Dockerfile ./artifacts --platform linux/amd64)"
