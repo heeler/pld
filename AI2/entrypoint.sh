594c09ce9fb89683b6607ef17a00065b2193c117
heeler <jamie.sherman@alleninstitute.org>
2024-11-12 15:46:58 -0800

    trying clone again

--- AI2/entrypoint.sh
+++ AI2/entrypoint.sh
@@ -3,9 +3,8 @@ set -e
 python3.12 -m venv env
 . env/bin/activate
 python3.12 -m pip install --upgrade pip
-/bin/bash
-#pip3.12 install git+https://github.com/heeler/pld.git
-#printf "install done\n running"
-#sleep 5
-#cuda_check
+pip3.12 install git+https://github.com/heeler/pld.git
+printf "install done\n running"
+sleep 5
+cuda_check
 echo "done"
